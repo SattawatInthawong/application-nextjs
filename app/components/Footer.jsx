11eb2cd115a7a41e815db8cfd72b55c540b1a7f7
SattawatInthawong <154236493+SattawatInthawong@users.noreply.github.com>
2025-08-04 14:11:16 +0700

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='bg-blue-700 text-white p-6'>
       <p className='text-center text-lg'>Copyright &copy; 2025</p>
-      <p className='text-center text-lg my-3'>cerate by Sattawat Inthawong</p>
+      <p className='text-center text-lg my-3'>create by Sattawat Inthawong</p>
     </footer>
   )
 }
